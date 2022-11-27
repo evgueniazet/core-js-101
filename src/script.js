c6fd5ae91627c241b8a1533c26d4827c529b145d
evgueniazet <zelenkoea19@gmail.com>
2022-11-27 14:51:24 +0200

    add: solved to array of squares in 03-array-tasks

--- src/script.js
+++
@@ -1,8 +0,0 @@
-const toCsvText = (arr) => {
-  const newArr = arr.map((item) => item.map((element) => element.toString()).join(',')).join('\n');
-  return newArr;
-};
-toCsvText([[0, 1, 2, 3, 4],
-  [10, 11, 12, 13, 14],
-  [20, 21, 22, 23, 24],
-  [30, 31, 32, 33, 34]]);
