d1db67c6d3dd53db790909b1b72cbd6d81da6967
evgueniazet <zelenkoea19@gmail.com>
2023-05-30 12:09:07 +0200

    feat: isPrime solved

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -211,12 +211,17 @@ const roundToPowerOfTen = (num, pow) => {
  *   17 => true
  */
 const isPrime = (n) => {
-  if (n % 2 === 0) {
+  if (n <= 1) {
     return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
   }
   return true;
 };
-
 
 /**
  * Tries to convert value to number and returns it if conversion was successful;
