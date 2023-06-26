4c12a9ae5bae3866770c0b99d37f801d353d5cc9
evgueniazet <zelenkoea19@gmail.com>
2023-06-26 12:30:56 +0200

    feat: Rectangle solved

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,16 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectangle = {
+    width,
+    height,
+    getArea() {
+      return rectangle.width * rectangle.height;
+    },
+  };
+
+  return rectangle;
 }
 
 
